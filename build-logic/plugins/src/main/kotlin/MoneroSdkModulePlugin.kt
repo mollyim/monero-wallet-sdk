cbe714ea3562ce5b1af2cd1b9147b4e40ea7fee3
Oscar Mira <valldrac@molly.im>
2025-05-29 17:32:21 +0200

    ci: add workflow to publish snapshots to Maven Central

--- build-logic/plugins/src/main/kotlin/MoneroSdkModulePlugin.kt
+++ build-logic/plugins/src/main/kotlin/MoneroSdkModulePlugin.kt
@@ -3,12 +3,24 @@ import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.kotlin.dsl.apply
 import org.gradle.kotlin.dsl.getByType
+import org.jetbrains.kotlin.com.google.gson.Gson
 import java.io.File
 
 class MoneroSdkModulePlugin : Plugin<Project> {
-    override fun apply(project: Project) = with(project) {
+    override fun apply(project: Project): Unit = with(project) {
         apply(plugin = "jacoco")
         configureJacoco(extensions.getByType<LibraryAndroidComponentsExtension>())
+
+        tasks.register("version") {
+            doLast {
+                val versionInfo = mapOf(
+                    "version" to version,
+                    "isSnapshot" to isSnapshot,
+                )
+
+                println(Gson().toJson(versionInfo))
+            }
+        }
     }
 }
 
